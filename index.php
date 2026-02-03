3d4b375f248bfc624733f51063c3f104e2742373
ebarbos <ebarbos.ebarbos@gmail.com>
2026-02-03 18:36:29 +0300

    edit file

--- index.php
+++ index.php
@@ -3,12 +3,13 @@ mb_internal_encoding('UTF-8');
 
 function normalizeNamePart(string $s): string {
     $s = trim(preg_replace('/\s+/u', ' ', $s));
-    return $s === '' ? '' : mb_ucfirst(mb_strtolower($s, 'UTF-8'));
+    return $s === '' ? '' : mb_convert_case($s, MB_CASE_TITLE, "UTF-8");
 }
 
-$firstName  = 'иван';
-$lastName   = 'иванов';
-$patronymic = 'иванович';
+// Чтение данных от пользователя
+$firstName  = readline("Введите имя: ");
+$lastName   = readline("Введите фамилию: ");
+$patronymic = readline("Введите отчество (если есть, иначе Enter): ");
 
 $firstNameN  = normalizeNamePart($firstName);
 $lastNameN   = normalizeNamePart($lastName);
@@ -20,16 +21,16 @@ $fullName = trim("$lastNameN $firstNameN $patronymicN");
 // Фамилия и инициалы
 $surnameAndInitials = $lastNameN;
 $initials = [];
-if ($firstNameN)  $initials[] = mb_substr($firstNameN, 0, 1) . '.';
-if ($patronymicN) $initials[] = mb_substr($patronymicN, 0, 1) . '.';
+if ($firstNameN)  $initials[] = mb_substr($firstNameN, 0, 1, "UTF-8") . '.';
+if ($patronymicN) $initials[] = mb_substr($patronymicN, 0, 1, "UTF-8") . '.';
 if ($initials) $surnameAndInitials .= ' ' . implode('', $initials);
 
 // Аббревиатура
 $fio = '';
-if ($lastNameN)  $fio .= mb_substr($lastNameN, 0, 1);
-if ($firstNameN) $fio .= mb_substr($firstNameN, 0, 1);
-if ($patronymicN) $fio .= mb_substr($patronymicN, 0, 1);
-$fio = mb_strtoupper($fio);
+if ($lastNameN)  $fio .= mb_substr($lastNameN, 0, 1, "UTF-8");
+if ($firstNameN) $fio .= mb_substr($firstNameN, 0, 1, "UTF-8");
+if ($patronymicN) $fio .= mb_substr($patronymicN, 0, 1, "UTF-8");
+$fio = mb_strtoupper($fio, "UTF-8");
 
 echo "Полное имя: $fullName\n";
 echo "Фамилия и инициалы: $surnameAndInitials\n";
